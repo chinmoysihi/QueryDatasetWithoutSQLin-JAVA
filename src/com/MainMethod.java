bcfae8af4334f2dd43fe81615c85243fca8e33cd
Chinmoy's <chinmoysihi@gmail.com>
2018-09-21 13:45:54 +0530

    Main

--- src/com/MainMethod.java
+++ src/com/MainMethod.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class MainMethod {
 	
-	public static final String CSV_deliveries="D:\\EclipseWorkSpace\\Querying_Data\\iplDataset\\deliveries.csv";
-	public static final String CSV_matches="D:\\EclipseWorkSpace\\Querying_Data\\iplDataset\\matches.csv";
+	public static final String CSV_deliveries="D:\\EclipseWorkSpace\\QueryDatasetWithoutSQLin-JAVA\\iplDataset\\deliveries.csv";
+	public static final String CSV_matches="D:\\EclipseWorkSpace\\QueryDatasetWithoutSQLin-JAVA\\iplDataset\\matches.csv";
 	static ArrayList dell=null;
 	static ArrayList matt=null;
 	public static void main(String[] args) {
@@ -17,15 +17,19 @@ public class MainMethod {
         System.out.println(" Matches read Completed");
         
         System.out.println();
+        System.out.println("Top 4 teams which elected to field first after winning toss in the year 2016 and 2017.");
         Top4TeamsFieldFirst.fieldFirst(matt);
         
         System.out.println();
+        System.out.println("List  of Total number of fours, sixes, total score with respect to team and year");
         TotalFourSixScore.score(dell, matt);
         
-        System.out.println();        
+        System.out.println();
+        System.out.println("Top 10 best economy rate bowler with respect to year who bowled at least 10 overs");
         TopTenEconomy.economy(dell, matt);
         
         System.out.println();
+        System.out.println("Team name which has Highest Net Run Rate with respect to year");
         TeamNetRunRate.netRunRate(dell, matt);
 
 	}
